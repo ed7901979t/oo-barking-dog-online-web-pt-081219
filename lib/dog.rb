eac09c94a8bb49101615ffb36dc9bf408d1bb94a
clean-command-1851 <ed7901979t@gmail.com>
2019-08-23 17:45:50 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,13 +1,15 @@
 # Your code goes here!
 class Dog   
   def name=(dogsname)
-    name=dogsname
+    @name=dogsname
   end
   def name
-    dogsname
+    @dogsname
   end
   
   def bark
     puts "woof!"
   end
 end 
+
+name="Leslie"
